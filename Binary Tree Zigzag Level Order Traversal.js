954c322acce6ac1633821c0a34dea9d6d1c8d2cb
Bhaba <bhabajyoti.kalita@dxsherpa.com>
2023-06-23 01:08:29 +0530

    Adding new file Leetcode 103

---
+++ Binary Tree Zigzag Level Order Traversal.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    
+    if(root == null) return [];
+
+    let q = [root];
+    let flag = 0;
+    let res = [];
+
+    while(q.length > 0) {
+
+        let curr = [];
+
+        let len = q.length;
+
+        for(let i=0; i<len; i++) {
+
+            let node = q.shift();
+
+            if(flag % 2 == 0) {
+                curr.push(node.val);
+
+            } else {
+                curr.unshift(node.val);
+            }
+
+            if(node.left) {
+                q.push(node.left);
+            }
+
+            if(node.right) {
+                q.push(node.right);
+            }
+        }
+        flag++;
+        res.push(curr);
+    }
+    return res;
+};
